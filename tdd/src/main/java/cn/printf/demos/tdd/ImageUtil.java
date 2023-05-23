70459d7b1648cede0e1243f1798aceb2ddc3ba21
lin <120377843@qq.com>
2023-05-23 14:12:10 +0800

    refactor image example

--- tdd/src/main/java/cn/printf/demos/tdd/ImageUtil.java
+++ tdd/src/main/java/cn/printf/demos/tdd/ImageUtil.java
@@ -22,35 +22,36 @@ public class ImageUtil {
 
         BufferedImage image = new BufferedImage(width, height,
                 BufferedImage.TYPE_INT_BGR);
-        Graphics g = image.getGraphics();
-        g.setClip(0, 0, width, height);
-        g.setColor(color);
-        g.fillRect(0, 0, width, height);
-        g.setColor(Color.white);
-        g.setFont(font);
-        Rectangle clip = g.getClipBounds();
-        FontMetrics fm = g.getFontMetrics(font);
+        Graphics graphics = image.getGraphics();
+        graphics.setClip(0, 0, width, height);
+        graphics.setColor(color);
+        graphics.fillRect(0, 0, width, height);
+        graphics.setColor(Color.white);
+        // 设置画笔字体
+        graphics.setFont(font);
+        Rectangle clip = graphics.getClipBounds();
+        FontMetrics fontMetrics = graphics.getFontMetrics(font);
 
-        int ascent = fm.getAscent();
-        int descent = fm.getDescent();
+        int ascent = fontMetrics.getAscent();
+        int descent = fontMetrics.getDescent();
         int y = (clip.height - (ascent + descent)) / 2 + ascent;
         int x = (width - fontSize * text.length()) / 2;
-        g.drawString(text, x, y);
-        g.dispose();
-
+        graphics.drawString(text, x, y);
+        graphics.dispose();
+        // 输出 jpg 图片
         return bufferedImageToBase64(image);
     }
 
     private static String bufferedImageToBase64(BufferedImage image) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         try {
-            ImageIO.write(image, "jpg", baos);
+            ImageIO.write(image, "jpeg", baos);
         } catch (IOException e) {
             e.printStackTrace();
         }
         byte[] bytes = baos.toByteArray();
         BASE64Encoder encoder = new BASE64Encoder();
         String base64 = encoder.encodeBuffer(bytes).trim();
-        return "data:image/jpg;base64," + base64;
+        return "data:image/jpeg;base64," + base64;
     }
 }
--- tdd/src/test/java/cn/printf/demos/tdd/ImageUtilTest.java
+++ tdd/src/test/java/cn/printf/demos/tdd/ImageUtilTest.java
@@ -12,7 +12,7 @@ public class ImageUtilTest {
 
     @Test
     public void should_generate_watermark_image() {
-        Assert.assertEquals("data:image/jpg;base64,/9j/4AAQSkZJRgABAgAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0a\n" +
+        Assert.assertEquals("data:image/jpeg;base64,/9j/4AAQSkZJRgABAgAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0a\n" +
                 "HBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIy\n" +
                 "MjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAA8ADwDASIA\n" +
                 "AhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQA\n" +
@@ -27,13 +27,13 @@ public class ImageUtilTest {
                 "Hv73Sr/U7eDfZ6f5f2mTeo8vzG2pwTk5I7A+9Mg0vULmCOeCxuZYZZxbRyRwsyvMRkRggYLEfw9a\n" +
                 "+n7PXRD4bu9Q1nVtf0a2fb5NxrENnDKCu4sscaoSWIU8MhJGNo71538U/F1vq1jayPZ+L9Hv4GEm\n" +
                 "nxXUCW8BkVhukPO8sFOAQflJHTJz85hc7xGIr+yVJWva6bdtF5K+u9tluur1lTSV7nmv/CF+Kv8A\n" +
-                "oWdZ/wDACX/4mquoeHta0mBZ9S0fULOFm2LJc2zxqWwTjLADOAePavZrC5srTwFq91N8UdTuLa+u\n" +
-                "YbSDUntrnfaypmRlVSxb5l4JGB7npXnnxCh1PTJ9Osp/Feoa9YXlnFqED3LSKuHLhTsdmwcDrwfm\n" +
-                "xXVg8yq163s5JLW3wzV7JN2uktL2s9dBSgkrnFUUUV7ZkFXtH1i/0DVYdT0yfyLyDd5cmxW27lKn\n" +
-                "hgR0J7VRoqZRjOLjJXTGe26N8TIr7wdZQ3nji50XXIp5jdSyaaLtbhWcsMDaQAAQBgrjBG3G0jD1\n" +
-                "/W/Cl5p9raat4r1zxG8uoPdXM9tHJB5MflMFRIpW8sHewIIXhdw44B8uoryYZLQp1HOm2rtvRRW/\n" +
-                "ny81l0Vy3UbVmeo3PiX4b3HhSy8OCLxTFYW07XJMUVqJJpDkBpG7kAlRjHGM5wMZ3jTxB4K1/SrT\n" +
-                "+zo9fTUdPsYbC0+0LCImjjbq+0k7tpbpgZxxXn9FbU8rpU5qalK6be/V7/eJzbVgooor0iAooooA\n" +
-                "KKKKACiiigAooooA/9k=", watermarkImage("老五", Color.BLUE));
+                "oWdZ/wDACX/4mquoeHta0mBZ9S0fULOFm2LJc2zxqWwTjLADOAePavarG7020+HeqXMnxO1adL28\n" +
+                "jtI9UeC4zbOmJNiJneCy7stkAggdufOPiFDqemT6dZT+K9Q16wvLOLUIHuWkVcOXCnY7Ng4HXg/N\n" +
+                "iunB5lWr1vZySWrXwzV7JN2urfJ6ilBJXOKooor3DIKvaPrF/oGqw6npk/kXkG7y5Nitt3KVPDAj\n" +
+                "oT2qjRUyjGcXGSumM9t0b4mRX3g6yhvPHFzouuRTzG6lk00Xa3Cs5YYG0gAAgDBXGCNuNpGHr+t+\n" +
+                "FLzT7W01bxXrniN5dQe6uZ7aOSDyY/KYKiRSt5YO9gQQvC7hxwD5dRXkwyWhTqOdNtXbeiit/Pl5\n" +
+                "rLorluo2rM9Yu/F/w6vPBVh4Ukh8UiwspzPHIq2/mliXOCd2MfvG7DoKx/GniDwVr+lWn9nR6+mo\n" +
+                "6fYw2Fp9oWERNHG3V9pJ3bS3TAzjivP6K1pZVSpTU4yldNvfq9/v6ic21YKKKK9MgKKKKACiiigA\n" +
+                "ooooAKKKKAP/2Q==", watermarkImage("老五", Color.BLUE));
     }
 }
